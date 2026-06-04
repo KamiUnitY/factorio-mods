8dc20bee07b75ecb3e03a9c53d37b56515437dda
KamiUnitY <kamiunity141@gmail.com>
2026-06-05 00:55:34 +0700

    Added ore-crushing graphics

--- kamy-graphics-override/data-updates.lua
+++ kamy-graphics-override/data-updates.lua
@@ -2,3 +2,10 @@ data.raw.item["burner-assembling-machine"].icon = "__kamy-graphics-override__/gr
 data.raw["assembling-machine"]["burner-assembling-machine"].icon = "__kamy-graphics-override__/graphics/icons/burner-assembling-machine.png"
 
 data.raw.technology["basic-fluid-handling"].icon = "__kamy-graphics-override__/graphics/technology/basic-fluid-handling.png"
+
+data.raw.technology["ore-crushing"].icon = "__kamy-graphics-override__/graphics/technology/ore-crushing.png"
+
+local ore_crushing_productivity_icons = util.technology_icon_constant_recipe_productivity("__kamy-graphics-override__/graphics/technology/ore-crushing.png")
+data.raw.technology["ore-crushing-productivity-1"].icons = ore_crushing_productivity_icons
+data.raw.technology["ore-crushing-productivity-2"].icons = ore_crushing_productivity_icons
+data.raw.technology["ore-crushing-productivity-3"].icons = ore_crushing_productivity_icons
