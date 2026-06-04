0d8f9ef46725fdeddac502a3661c3297dc628469
KamiUnitY <kamiunity141@gmail.com>
2026-06-05 00:59:11 +0700

    Check if mod installed for graphics override

--- kamy-graphics-override/data-updates.lua
+++ kamy-graphics-override/data-updates.lua
@@ -1,11 +1,17 @@
-data.raw.item["burner-assembling-machine"].icon = "__kamy-graphics-override__/graphics/icons/burner-assembling-machine.png"
-data.raw["assembling-machine"]["burner-assembling-machine"].icon = "__kamy-graphics-override__/graphics/icons/burner-assembling-machine.png"
+if mods["aai-industry"] then
+    data.raw.item["burner-assembling-machine"].icon = "__kamy-graphics-override__/graphics/icons/burner-assembling-machine.png"
+    data.raw["assembling-machine"]["burner-assembling-machine"].icon = "__kamy-graphics-override__/graphics/icons/burner-assembling-machine.png"
 
-data.raw.technology["basic-fluid-handling"].icon = "__kamy-graphics-override__/graphics/technology/basic-fluid-handling.png"
+    data.raw.technology["basic-fluid-handling"].icon = "__kamy-graphics-override__/graphics/technology/basic-fluid-handling.png"
+end
 
-data.raw.technology["ore-crushing"].icon = "__kamy-graphics-override__/graphics/technology/ore-crushing.png"
+if mods["crushing-industry"] then
+    data.raw.technology["ore-crushing"].icon = "__kamy-graphics-override__/graphics/technology/ore-crushing.png"
+end
 
-local ore_crushing_productivity_icons = util.technology_icon_constant_recipe_productivity("__kamy-graphics-override__/graphics/technology/ore-crushing.png")
-data.raw.technology["ore-crushing-productivity-1"].icons = ore_crushing_productivity_icons
-data.raw.technology["ore-crushing-productivity-2"].icons = ore_crushing_productivity_icons
-data.raw.technology["ore-crushing-productivity-3"].icons = ore_crushing_productivity_icons
+if mods["crushing-industry-productivity-research"] then
+    local ore_crushing_productivity_icons = util.technology_icon_constant_recipe_productivity("__kamy-graphics-override__/graphics/technology/ore-crushing.png")
+    data.raw.technology["ore-crushing-productivity-1"].icons = ore_crushing_productivity_icons
+    data.raw.technology["ore-crushing-productivity-2"].icons = ore_crushing_productivity_icons
+    data.raw.technology["ore-crushing-productivity-3"].icons = ore_crushing_productivity_icons
+end
