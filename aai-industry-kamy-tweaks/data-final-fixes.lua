95ee7a23213416cf982ef70a10396f043f0f6295
KamiUnitY <kamiunity141@gmail.com>
2026-07-19 01:21:53 +0700

    Moved changes from patch to aai mod

--- aai-industry-kamy-tweaks/data-final-fixes.lua
+++ aai-industry-kamy-tweaks/data-final-fixes.lua
@@ -1,3 +1,4 @@
 require("compatibility.Krastorio2")
 require("compatibility.Krastorio2-spaced-out")
 require("compatibility.crushing-industry")
+require("compatibility.FluidMustFlow")
--- kamy-vanilla-plus-patches/data-final-fixes.lua
+++ kamy-vanilla-plus-patches/data-final-fixes.lua
@@ -9,16 +9,3 @@ if mods["crushing-industry"] then
     util.technology_remove_recipe_effect_from_all("big-crusher")
     util.technology_insert_recipe_last("tungsten-steel", "big-crusher")
 end
-
-if mods["aai-industry"] and mods["crushing-industry"] then
-    util.technology_remove_recipe_effect_from_all("burner-crusher")
-    util.technology_insert_recipe_last("sand-processing", "burner-crusher")
-
-    util.technology_remove_recipe_effect_from_all("electric-crusher")
-    if settings.startup["crushing-industry-ore"].value then
-        data.raw.technology["ore-crushing"].prerequisites = {"advanced-material-processing"}
-        util.technology_insert_recipe_last("ore-crushing", "electric-crusher")
-    else
-        util.technology_insert_recipe_last("advanced-material-processing", "electric-crusher")
-    end
-end
