a0ea668da6daa2fd42f104154759abb812d030e7
KamiUnitY <kamiunity141@gmail.com>
2026-07-23 21:22:34 +0700

    Added enriching-industry to Vanilla+

--- kamy-vanilla-plus-patches/data-final-fixes.lua
+++ kamy-vanilla-plus-patches/data-final-fixes.lua
@@ -9,3 +9,28 @@ if mods["crushing-industry"] then
     util.technology_remove_recipe_effect_from_all("big-crusher")
     util.technology_insert_recipe_last("tungsten-steel", "big-crusher")
 end
+
+if mods["enriching-industry"] then
+    data:extend({
+        {
+            type = "item-subgroup",
+            name = "raw-resource-enriching",
+            group = "intermediate-products",
+            order = "b-m"
+        }
+    })
+
+    data.raw.recipe["ei-quartz"].subgroup = "raw-resource-enriching"
+    data.raw.recipe["ei-enriched-iron-ore"].subgroup = "raw-resource-enriching"
+    data.raw.recipe["ei-sulfuric-iron-leaching"].subgroup = "raw-resource-enriching"
+    data.raw.recipe["ei-enriched-iron-ore-recrystallization"].subgroup = "raw-resource-enriching"
+    data.raw.recipe["ei-enriched-copper-ore"].subgroup = "raw-resource-enriching"
+    data.raw.recipe["ei-sulfuric-copper-leaching"].subgroup = "raw-resource-enriching"
+    data.raw.recipe["ei-enriched-copper-ore-recrystallization"].subgroup = "raw-resource-enriching"
+    data.raw.recipe["ei-tailing-slurry-filtering"].subgroup = "raw-resource-enriching"
+    data.raw.recipe["ei-tailing-slurry-reprocessing"].subgroup = "raw-resource-enriching"
+
+    data.raw.recipe["ei-quartz-smelting"].order = "a[smelting]-d[glass]-a[quartz]"
+    data.raw.recipe["ei-tailing-slurry-reprocessing"].order = "f[tailing-slurry-reprocessing]"
+    data.raw.recipe["ei-tailing-slurry-filtering"].order = "f[tailing-slurry-filtering]"
+end
