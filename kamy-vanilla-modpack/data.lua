8bd48f5381d4d0cf664620f2a8a9ef5bcad21d09
KamiUnitY <kamiunity141@gmail.com>
2026-07-24 21:15:54 +0700

    Changed duct balances

---
+++ kamy-vanilla-modpack/data.lua
@@ -0,0 +1,46 @@
+local util = require("libraries/utility")
+
+if mods["FluidMustFlowThruTungsten"] then
+    util.recipe_clear_ingredients("duct-small", {
+        {type = "item", name = "tungsten-plate"},
+    })
+    util.recipe_set_ingredient_amount("duct-small", {
+        {type = "item", name = "tungsten-carbide", amount = 2}
+    })
+
+    util.recipe_clear_ingredients("duct-underground", {
+        {type = "item", name = "tungsten-plate"},
+    })
+    util.recipe_set_ingredient_amount("duct-underground", {
+        {type = "item", name = "tungsten-carbide", amount = 10},
+        {type = "item", name = "duct-small", amount = 30}
+    })
+
+    util.recipe_clear_ingredients("duct-t-junction", {
+        {type = "item", name = "tungsten-plate"},
+    })
+    util.recipe_set_ingredient_amount("duct-t-junction", {
+        {type = "item", name = "tungsten-carbide", amount = 4}
+    })
+
+    util.recipe_clear_ingredients("duct-curve", {
+        {type = "item", name = "tungsten-plate"},
+    })
+    util.recipe_set_ingredient_amount("duct-curve", {
+        {type = "item", name = "tungsten-carbide", amount = 4}
+    })
+
+    util.recipe_clear_ingredients("duct-cross", {
+        {type = "item", name = "tungsten-plate"},
+    })
+    util.recipe_set_ingredient_amount("duct-cross", {
+        {type = "item", name = "tungsten-carbide", amount = 4}
+    })
+
+    util.recipe_clear_ingredients("non-return-duct", {
+        {type = "item", name = "tungsten-plate"},
+    })
+    util.recipe_set_ingredient_amount("non-return-duct", {
+        {type = "item", name = "tungsten-carbide", amount = 4}
+    })
+end
