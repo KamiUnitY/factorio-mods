2a06027367250b25f9bfc1ee2fadd91e8ab6753c
KamiUnitY <kamiunity141@gmail.com>
2026-07-27 05:17:32 +0700

    Remove burner-crusher reskin for 2.1

--- kamy-reskins/reskins.lua
+++ kamy-reskins/reskins.lua
@@ -56,15 +56,15 @@ return function ()
             data.raw.technology["ore-crushing"].icon = "__kamy-reskins__/graphics/technology/ore-crushing.png"
         end
 
-        data.raw.item["burner-crusher"].icon = "__kamy-reskins__/graphics/icons/burner-crusher.png"
+        -- data.raw.item["burner-crusher"].icon = "__kamy-reskins__/graphics/icons/burner-crusher.png"
         data.raw.item["electric-crusher"].icon = "__kamy-reskins__/graphics/icons/electric-crusher.png"
         data.raw.item["big-crusher"].icon = "__kamy-reskins__/graphics/icons/big-crusher.png"
 
-        data.raw["furnace"]["burner-crusher"].icon = "__kamy-reskins__/graphics/icons/burner-crusher.png"
+        -- data.raw["furnace"]["burner-crusher"].icon = "__kamy-reskins__/graphics/icons/burner-crusher.png"
         data.raw["furnace"]["electric-crusher"].icon = "__kamy-reskins__/graphics/icons/electric-crusher.png"
         data.raw["assembling-machine"]["big-crusher"].icon = "__kamy-reskins__/graphics/icons/big-crusher.png"
 
-        data.raw["furnace"]["burner-crusher"].graphics_set.animation.layers[1].filename = "__kamy-reskins__/graphics/entity/burner-crusher/burner-crusher-base.png"
+        -- data.raw["furnace"]["burner-crusher"].graphics_set.animation.layers[1].filename = "__kamy-reskins__/graphics/entity/burner-crusher/burner-crusher-base.png"
         data.raw["furnace"]["electric-crusher"].graphics_set.animation.layers[1].filename = "__kamy-reskins__/graphics/entity/electric-crusher/electric-crusher-base.png"
         data.raw["assembling-machine"]["big-crusher"].graphics_set.animation.layers[2].filename = "__kamy-reskins__/graphics/entity/big-crusher/big-crusher-base.png"
     end
